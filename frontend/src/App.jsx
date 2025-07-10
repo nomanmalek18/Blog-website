e094e8783db63b78878a1aadc76edd950890d7a1
nomanmalek18 <nomanmalek2043@gmail.com>
2025-07-10 20:15:02 +0530

    Blog Website

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,6 @@ import Comments from './pages/Comment'
 import Signup from './pages/Singup'
 import Login from './pages/Login'
 import Profile from './pages/Profile'
-
 const router = createBrowserRouter([
   {
     path: "/",
