315eb29e806b3b204a7951fee74d7bf9bb0a0256
nomanmalek18 <nomanmalek2043@gmail.com>
2025-07-13 13:37:12 +0530

    Blog Website

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended:true}));
 app.use(cors({
-    origin: "https://blog-website-67nw.onrender.com",
+    origin: "http:localhost:8000",
     credentials:true
 }))
 
